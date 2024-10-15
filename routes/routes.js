3a92f214527052d56e16d3ff7acf99844456ed2a
Luis Lima <luisfernando.lima2005@gmail.com>
2024-10-15 18:57:20 -0300

    Criação da página de cadastro

--- routes/routes.js
+++ routes/routes.js
@@ -5,4 +5,10 @@ router.get("/", (req, res) => {
   res.render('home', {css: "home.css"})
 });
 
+router.get('/cad', (req, res) => {
+  res.render('formCadastro', {css: "cad.css"})
+})
+
+
+
 module.exports = router;
